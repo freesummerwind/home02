57455805315b0045887b08219ee2b12dffc04c3a
freesummerwind <Liz-e-tta@yandex.ru>
2020-03-30 17:24:20 +0300

    Change text a little

--- hello_world.cpp
+++ hello_world.cpp
@@ -7,5 +7,5 @@ int main()
   cout << "Hello! Input your name: ";
   std::string name = "";
   cin >> name;
-  cout << "Hello World form " << name << endl;
+  cout << "Hello World from " << name << endl;
 }
