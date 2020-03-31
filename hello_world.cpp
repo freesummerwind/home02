149434f30f9340a165b681cdef03544489c8b181
freesummerwind <61392605+freesummerwind@users.noreply.github.com>
2020-03-31 20:03:20 +0300

    second changing of comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-  std::cout << "Hello! Input your name: "; //Запрашиваем имя пользователя
-  std::string name = ""; //Создаём переменную для хранения имени
-  std::cin >> name; //Сохраняем имя в переменную
-  std::cout << "Hello World from " << name << std::endl; //Выводим приветствие
+  std::cout << "Hello! Input your name: "; //Запрашиваем имя пользователя.
+  std::string name = ""; //Создаём переменную для хранения имени.
+  std::cin >> name; //Сохраняем имя в переменную.
+  std::cout << "Hello World from " << name << std::endl; //Выводим приветствие.
 }
